1ec453964a6973a297b37defcd85c9e5ea3c0ca4
WilsonLi564 <115434167+WilsonLi564@users.noreply.github.com>
2023-11-03 15:02:32 -0700

    TubeCalcv2 on JupyterLabs
    Still requires units to be checked

--- Tube Calculator/test.py
+++ Tube Calculator/test.py
@@ -1,19 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import fluids as fl
 
-a = np.array([1, 2, 3])
-b = np.array([3, 2, 1])
+peepee = [9, 6, 3]
+poopoo = 3
 
-fig, ax = plt.subplots()
-lines = ax.plot(a, b)
-plt.show()
+print(np.divide(poopoo, peepee))
 
-def haaland(Re, e, D):
-    return np.power((1/(-1.8 * np.log10(np.power((e/D)/3.7, 1.11) + (6.9/Re)))), 2)
-
-print(np.square(a))
-print(np.divide(a, b))
-print (2 * a)
-
-print(haaland(100, 0.001, 0.5))
-print(np.power(2, 2))
+#print(fl.vectorized.Reynolds(V=[1, 2, 3], mu = 1, rho = 1, D = 1))
